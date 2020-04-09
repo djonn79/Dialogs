70e6479e4262887041bff79cae3665f7bf33179a
Alx <bender.rdgz.2996@gmail.com>
2020-04-09 14:27:52 +1000

    Добавьте файлы проекта.

---
+++ Dialogs/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dialogs
+{
+    public enum DialogResult { None, Yes, No, Ok, Cancel }
+
+    public enum WindowType { Information, Error, Exclamation, Warning, CriticalError, Question }
+}
---
+++ Dialogs/ViewModel/DialogVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dialogs.ViewModel
+{
+    public class DialogVM
+    {
+        public DialogResult DialogResult { get; set; }
+        public WindowType WindowType { get; set; }
+    }
+}
