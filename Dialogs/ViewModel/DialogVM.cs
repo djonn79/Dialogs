285c7ec343bf4357de78535594b19c12081bc816
djonn79 <zheka.zaicev@gmail.com>
2020-04-22 09:42:51 +1100

    r

--- Dialogs/ViewModel/DialogVM.cs
+++ Dialogs/ViewModel/DialogVM.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dialogs.ViewModel
 {
-    public class DialogVM
+    public class DialogVM : ViewModelBase
     {
         public DialogResult DialogResult { get; set; }
         public WindowType WindowType { get; set; }
