6fb17b4fd257e6a9595249479e7ad4663b24421b
Alx <bender.rdgz.2996@gmail.com>
2020-04-22 12:21:02 +1000

    Добавил RelayCommand

---
+++ Dialogs/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Dialogs
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute?.Invoke(parameter);
+        }
+
+        public Predicate<object> canExecute { get; set; }
+        public Action<object> execute { get; private set; }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+    }
+}
